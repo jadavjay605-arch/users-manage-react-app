3972cbed7c64f41d1df349319d6a87612322ae30
jadavjay605-arch <jadavjay605@gmail.com>
2026-02-12 11:13:10 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import { getLoggedUser } from "./utils/auth";
+import type { ReactNode } from "react";
+
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  return getLoggedUser() ? children : <Navigate to="/login" />;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
